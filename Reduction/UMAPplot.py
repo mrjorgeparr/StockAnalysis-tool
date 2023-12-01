fce9a0c0fa4992f81d0199162540e8d368447d99
Jorge Parreño <jorpahermar@gmail.com>
2023-12-02 00:21:55 +0100

    adding fetsbst

--- Reduction/UMAPplot.py
+++ Reduction/UMAPplot.py
@@ -1,49 +1,101 @@
-import umap 
+import umap
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import os
-from sklearn.datasets import make_blobs
-
+from sklearn.decomposition import KernelPCA
 
 class UMAPplot:
-    """
-    Preliminary design of a UMAPplot class to quickly
-    assess how good our data separation is
-    """
-    def __init__(self, features, labels, n_comp):
+    def __init__(self, features, labels, n_comp, applyKernel=True):
         self.data = features
         self.labels = labels
-        assert n_comp in [2,3], "invalid number of components"
+        self.n_comp = n_comp
+        self.selected_features = None  # Store selected features for printing
+        assert n_comp in [2, 3], "invalid number of components"
         self.umodel = umap.UMAP(n_components=n_comp)
         self.umres = self.umodel.fit_transform(self.data)
+        
+        if applyKernel:
+            self.kernel = KernelPCA(n_components=n_comp, kernel='poly', degree=3)
+            self.umres = self.kernel.fit_transform(self.umres)
 
-    def plot(self):
+    def plot(self, ax=None):
         if self.umres.shape[1] == 2:
-            plt.scatter(self.umres[:, 0], self.umres[:, 1], c=self.labels, cmap='viridis')
-            plt.title('UMAP Projection (2D)')
-            plt.xlabel('UMAP Component 1')
-            plt.ylabel('UMAP Component 2')
-            plt.colorbar()
-            plt.show()
+            if ax is None:
+                plt.scatter(self.umres[:, 0], self.umres[:, 1], c=self.labels, cmap='viridis')
+                plt.title('UMAP Projection (2D)')
+                plt.xlabel('UMAP Component 1')
+                plt.ylabel('UMAP Component 2')
+            else:
+                ax.scatter(self.umres[:, 0], self.umres[:, 1], c=self.labels, cmap='viridis')
+                ax.set_title('UMAP Projection (2D)')
+                ax.set_xlabel('UMAP Component 1')
+                ax.set_ylabel('UMAP Component 2')
+
         elif self.umres.shape[1] == 3:
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            sc = ax.scatter(self.umres[:, 0], self.umres[:, 1], self.umres[:, 2], c=self.labels, cmap='viridis')
-            plt.title('UMAP Projection (3D)')
-            ax.set_xlabel('UMAP Component 1')
-            ax.set_ylabel('UMAP Component 2')
-            ax.set_zlabel('UMAP Component 3')
-            plt.colorbar(sc)
-            plt.show()
+            raise ValueError("3D plotting not supported for this example")
         else:
             raise ValueError("Invalid number of components for plotting")
 
 if __name__ == "__main__":
+    df = pd.read_csv('./../Dataset/scaledData.csv')
+    ################################# TO STUDY FEATURE SUBSETS WITH BETTER SEPARATION ##################################################
+    
+    df.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis=1, inplace=True)
+    target = 'discretized FADY'
+    target = df.columns.tolist()[-1]
+    y = df[target]
+    X = df.drop(columns=[target, 'Ticker'], axis=1)
+    
+    from sklearn.impute import SimpleImputer
+    imputer = SimpleImputer(strategy='mean')
+    X_imputed = pd.DataFrame(imputer.fit_transform(X), columns=X.columns)
+
+    # Create a 6x5 grid plot for 30 random selections of 5 features
+    fig, axs = plt.subplots(6, 5, figsize=(20, 20))
+
+    for i in range(6):
+        for j in range(5):
+            # Select 5 random features
+            selected_feature_names = np.random.choice(X.columns, size=5, replace=False)
+            X_selected = X_imputed[selected_feature_names]
+
+            # Print selected feature names to the terminal
+            print(f"Selected Features for Plot {i*5 + j + 1}: {', '.join(selected_feature_names)}")
+
+            # Initialize UMAPplot for the selected features
+            umpPlotter = UMAPplot(X_selected, y, 2, applyKernel=False)
+            
+            # Plot the UMAP projection without kernel
+            umpPlotter.plot(ax=axs[i, j])
+
+    plt.tight_layout()
+    plt.show()
+    
+    # umpPlotter.plot()
+   
     """
+    print(df.head())
+    ############### SCALING DATA  AND DISCRETIZING ############
+    df2 = df.copy()
+    print(df2.columns)
+    df2.drop('Ticker', axis=1, inplace=True) 
+    scaler = StandardScaler()
+    df2[df2.columns] = scaler.fit_transform(df2[df2.columns])
+    df2['Ticker'] = df['Ticker']
+    target = 'Forward_Annual_Dividend_Yield'
+    n_bins=5
+    df2[target] = pd.to_numeric(df2[target], errors='coerce')
+    df2[target].fillna(0, inplace=True)  # You can replace 0 with another appropriate value
+
+    df2['discretized FADY'] = pd.cut(df2[target], bins=n_bins, labels=False)
+    df2.drop(target, axis=1, inplace=True)
+    df2.to_csv('./../Dataset/scaledData.csv')
     """
-    nfet = 15
-    X, y = make_blobs(n_samples=300, centers=3, n_features=nfet, random_state=42)
+
+
+    """
+    nfet = 14
+    X, y = make_blobs(n_samples=100, centers=3, n_features=nfet, random_state=42)
 
     # Create a DataFrame from the generated data
     columns = [f"Feature {i+1}" for i in range(nfet)]
@@ -51,5 +103,7 @@ if __name__ == "__main__":
     df['Label'] = y
 
     # Test the UMAPplot class
-    umap_plotter = UMAPplot(features=df[columns], labels=df['Label'], n_comp=2)
+    umap_plotter = UMAPplot(features=df[columns], labels=df['Label'], n_comp=2, applyKernel=False)
     umap_plotter.plot()
+    """
+    
---
+++ Reduction/main.py
@@ -0,0 +1,38 @@
+from UMAPplot import UMAPplot
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    df = pd.read_csv('./../Dataset/scaledData.csv')
+    features = ['Operating_Cash_Flow', 'Return_on_Equity', 'Total_Cash_(mrq)', 'Total_Debt/Equity_(mrq)', '5_Year_Average_Dividend_Yield']
+    fet2 = ['5_Year_Average_Dividend_Yield', 'Payout_Ratio', 'Total_Debt/Equity_(mrq)', 'Return_on_Equity', 'Trailing_Annual_Dividend_Yield']
+    fet3 = ['Return_on_Equity', 'Total_Debt/Equity_(mrq)', 'Payout_Ratio', 'Trailing_Annual_Dividend_Yield', 'Total_Cash_(mrq)']
+    fet4 = ['Trailing_Annual_Dividend_Yield', 'Return_on_Equity', 'Total_Debt/Equity_(mrq)', 'Payout_Ratio', 'Profit_Margin']
+    fet9 = ['Return_on_Equity', 'Total_Cash_(mrq)', 'Trailing_Annual_Dividend_Yield', '5_Year_Average_Dividend_Yield', 'Profit_Margin']
+    fet14 = ['Total_Cash_(mrq)', '5_Year_Average_Dividend_Yield', 'Trailing_Annual_Dividend_Yield', 'Operating_Cash_Flow', 'Payout_Ratio']
+    fet17 = ['Levered_Free_Cash_Flow', 'Total_Cash_(mrq)', 'Payout_Ratio', 'Trailing_Annual_Dividend_Yield', 'Operating_Cash_Flow']
+    fet20 = ['Return_on_Equity', 'Total_Debt/Equity_(mrq)', 'Total_Cash_(mrq)', 'Levered_Free_Cash_Flow', '5_Year_Average_Dividend_Yield']
+    fet21 = ['Levered_Free_Cash_Flow', 'Profit_Margin', 'Total_Cash_(mrq)', 'Payout_Ratio', '5_Year_Average_Dividend_Yield']
+    fet22 = ['Levered_Free_Cash_Flow', 'Total_Cash_(mrq)', 'Payout_Ratio', 'Trailing_Annual_Dividend_Yield', 'Operating_Cash_Flow']
+    fet25 = ['Trailing_Annual_Dividend_Yield', '5_Year_Average_Dividend_Yield', 'Return_on_Equity', 'Levered_Free_Cash_Flow', 'Total_Cash_(mrq)']
+    fet26 = ['Return_on_Equity', 'Trailing_Annual_Dividend_Yield', 'Total_Debt/Equity_(mrq)', 'Total_Cash_(mrq)', '5_Year_Average_Dividend_Yield']
+    fet28 = ['Trailing_Annual_Dividend_Yield', '5_Year_Average_Dividend_Yield', 'Total_Debt/Equity_(mrq)', 'Return_on_Equity', 'Operating_Cash_Flow']
+    print(df.columns)
+    df.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis=1, inplace=True)
+    target = 'discretized FADY'
+    
+    for fet in [(fet2, 2), (fet3, 3),(fet4, 4), (fet9, 9), (fet14, 14), (fet17, 17), (fet20,20), (fet21, 21), (fet22, 22), (fet25, 25), (fet26, 26), (fet28, 28)]:
+        features, idx = fet
+        dfs = df[features]
+        dfs.to_csv('./../Dataset/v'+str(idx)+'.csv')
+        y = df[target]                                        
+        X = df.drop([target, 'Ticker'], axis=1)
+        from sklearn.impute import SimpleImputer
+        imputer = SimpleImputer(strategy='mean')
+        X_imputed = pd.DataFrame(imputer.fit_transform(X), columns=X.columns)
+
+        ump = UMAPplot(X_imputed, y,2, applyKernel=False)
+        ump.plot()
+        plt.show()
+    
