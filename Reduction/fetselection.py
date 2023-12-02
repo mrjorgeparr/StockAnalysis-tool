cb33e1e12f4210c0d3426b5c9cfd08d779e7f2e2
Jorge Parreño <jorpahermar@gmail.com>
2023-12-02 13:20:36 +0100

    saving UMAP result for feature selection

--- Reduction/fetselection.py
+++ Reduction/fetselection.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     df = pd.read_csv('./../Dataset/scaledData.csv')
     features = ['Operating_Cash_Flow', 'Return_on_Equity', 'Total_Cash_(mrq)', 'Total_Debt/Equity_(mrq)', '5_Year_Average_Dividend_Yield']
     fet2 = ['5_Year_Average_Dividend_Yield', 'Payout_Ratio', 'Total_Debt/Equity_(mrq)', 'Return_on_Equity', 'Trailing_Annual_Dividend_Yield']
+
+    # this one looks good
     fet3 = ['Return_on_Equity', 'Total_Debt/Equity_(mrq)', 'Payout_Ratio', 'Trailing_Annual_Dividend_Yield', 'Total_Cash_(mrq)']
     fet4 = ['Trailing_Annual_Dividend_Yield', 'Return_on_Equity', 'Total_Debt/Equity_(mrq)', 'Payout_Ratio', 'Profit_Margin']
     fet9 = ['Return_on_Equity', 'Total_Cash_(mrq)', 'Trailing_Annual_Dividend_Yield', '5_Year_Average_Dividend_Yield', 'Profit_Margin']
@@ -18,12 +20,13 @@ if __name__ == "__main__":
     fet25 = ['Trailing_Annual_Dividend_Yield', '5_Year_Average_Dividend_Yield', 'Return_on_Equity', 'Levered_Free_Cash_Flow', 'Total_Cash_(mrq)']
     fet26 = ['Return_on_Equity', 'Trailing_Annual_Dividend_Yield', 'Total_Debt/Equity_(mrq)', 'Total_Cash_(mrq)', '5_Year_Average_Dividend_Yield']
     fet28 = ['Trailing_Annual_Dividend_Yield', '5_Year_Average_Dividend_Yield', 'Total_Debt/Equity_(mrq)', 'Return_on_Equity', 'Operating_Cash_Flow']
-    print(df.columns)
+    # print(df.columns)
     df.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis=1, inplace=True)
     target = 'discretized FADY'
     
     for fet in [(fet2, 2), (fet3, 3),(fet4, 4), (fet9, 9), (fet14, 14), (fet17, 17), (fet20,20), (fet21, 21), (fet22, 22), (fet25, 25), (fet26, 26), (fet28, 28)]:
         features, idx = fet
+        features.append(target)
         dfs = df[features]
         dfs.to_csv('./../Dataset/v'+str(idx)+'.csv')
         y = df[target]                                        
---
+++ Reduction/reducer.py
@@ -0,0 +1,22 @@
+from umap import UMAP
+import pandas as pd
+import os
+import copy
+
+if __name__ == "__main__":
+    # df = pd.read_csv('./../Dataset/scaledData.csv')
+    base = './../Dataset'
+    files = [f for f in os.listdir(base) if f[0] == 'v']
+    for f in files:
+        df = pd.read_csv(os.path.join(base, f))
+        ump = UMAP(n_components=2, random_state=42, n_jobs=1)
+        df2 = df.copy()
+        df2.drop('discretized FADY', axis=1)
+        from sklearn.impute import SimpleImputer
+        imputer = SimpleImputer(strategy='mean')
+        X_imputed = pd.DataFrame(imputer.fit_transform(df2), columns=df2.columns)
+
+        embedded = pd.DataFrame(ump.fit_transform(X_imputed), columns = ['Umap 1', 'Umap 2'])
+        embedded['discretized FADY'] = df['discretized FADY']
+        embedded.to_csv(os.path.join(base, 'U' + f))
+        
