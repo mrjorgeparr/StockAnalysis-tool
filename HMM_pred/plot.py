a33b3f474e0d598e4dcd21e6bbfb54ca834e4fb1
Riccardo Conforto Galli <cg.riccardo@tuta.io>
2023-12-13 09:53:22 +0100

    remove unused files

--- HMM_pred/plot.py
+++ HMM_pred/plot.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statistics
 
 df = pd.read_csv('../Dataset/pred.csv')
 print(df.dtypes)
@@ -15,8 +16,19 @@ print(df)
 
 fig.suptitle("November closing price prediction", fontsize=30)
 
+perc_error = []
 for idx, company in enumerate(companies):
-    axes[idx].set_title(company)
+    pred = df[(df['Ticker'] == company) & (df['Type'] == 'prediction')]
+    real = df[(df['Ticker'] == company) & (df['Type'] == 'real')]
+    for _, p in pred.iterrows():
+        day = p['Date']
+        pval = p['Close']
+        rval = real[real['Date']==day]
+        rval = rval.iloc[0]['Close']
+        perc_error.append((pval-rval)/rval * 1000)        #pe = p[]
+
+
+    axes[idx].set_title(company+ ' prediction error:'+str(  round(statistics.mean(perc_error),2)) + '%')
     sns.lineplot(data=df[df['Ticker'] == company], x="Date", y="Close", hue="Type",ax = axes[idx])
 fig.tight_layout()
 fig.subplots_adjust(top=0.88)
