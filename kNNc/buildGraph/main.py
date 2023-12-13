4eff0b9b74bab13409760d9d5dfafbc003bb268a
Jorge Parreño <jorpahermar@gmail.com>
2023-12-13 18:02:56 +0100

    updated meaningful subsets to 100

--- kNNc/buildGraph/main.py
+++ kNNc/buildGraph/main.py
@@ -2,14 +2,14 @@ from baseBuilder import baseBuilder
 from GPclass import GPclass
 import pandas as pd
 import os
-from sklearn.model_selection import train_test_split
+
 
 ############ THE PURPOSE OF THE SCRIPT IS TO BUILD THE FOLDERS FOR THE REDUCED SUBSETS CORRESPONDING
 ############ TO THE DIFFERENT CENTRALITY MEASURES
 
 if __name__ == "__main__":
     base = './../../Dataset'
-    tosave = os.path.join(base, 'meaningfulSetsp')
+    tosave = os.path.join(base, 'meaningfulSetsb')
     os.makedirs(tosave)
     target = 'discretized FADY'
     files = [f for f in os.listdir(os.path.join(base, 'umap reduced'))]
@@ -21,8 +21,8 @@ if __name__ == "__main__":
         y = df[target]
         X = df.drop(target, axis=1)
         # this is the equivalent of a test train split, because we know there are 83 samples in our dataset
-        df = df.iloc[:66,:]
-        bf.ccrm("page_rank", df.values, VERBOSE=False)
+        df = df.iloc[:80,:]
+        bf.ccrm("betweenness", df.values, VERBOSE=False)
         nodes = list(bf.graph.nodes)
         msbs = df.iloc[nodes, :]
         msbs.to_csv(os.path.join(tosave, 'r'+f))
